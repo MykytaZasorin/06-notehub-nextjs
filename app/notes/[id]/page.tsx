f6b1fe994da24bc7bad6010e84ba088bbe417935
MykytaZasorin <mykytazasorin@gmail.com>
2026-02-10 23:51:42 +0200

    fix

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,23 +1,15 @@
-import { QueryClient } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 import { Note } from "@/types/note";
 
-interface Params {
-  id: string | string[];
+interface Props {
+  params: { id: string };
 }
 
-export default async function NoteDetailsPage({ params }: { params: Params }) {
-  const id = Array.isArray(params.id) ? params.id[0] : params.id;
+export default async function NoteDetailsPage({ params }: Props) {
+  // Отримуємо нотатку на сервері
+  const note: Note = await fetchNoteById(params.id);
 
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
-  });
-
-  const initialData = queryClient.getQueryData<Note>(["note", id]);
-
-  return <NoteDetailsClient noteId={id} initialData={initialData} />;
+  // Передаємо у клієнтський компонент як initialData
+  return <NoteDetailsClient noteId={params.id} initialData={note} />;
 }
